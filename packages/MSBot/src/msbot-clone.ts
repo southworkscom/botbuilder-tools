a90be9d592ca2488b8392ad88fe3dc077b8876ba
Victor Grycuk <victor@grycuk.net>
2018-09-11 12:08:59 -0300

    Disable TSLint rules filewise

--- packages/MSBot/src/msbot-clone.ts
+++ packages/MSBot/src/msbot-clone.ts
@@ -2,6 +2,7 @@
  * Copyright(c) Microsoft Corporation.All rights reserved.
  * Licensed under the MIT License.
  */
+/* tslint:disable */
 import { AppInsightsService, BlobStorageService, BotConfiguration,
         BotRecipe, BotService, CosmosDbService, DispatchService,
         EndpointService, FileService, GenericService, IBlobResource,
