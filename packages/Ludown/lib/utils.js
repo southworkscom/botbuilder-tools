eb46d5692e979553ec52fba43c93b36b4f00d869
Victor Grycuk <victor@grycuk.net>
2018-09-20 14:27:21 -0300

    Use package.name instead of path.dirname

--- packages/Ludown/lib/utils.js
+++ packages/Ludown/lib/utils.js
@@ -4,10 +4,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * Copyright(c) Microsoft Corporation.All rights reserved.
  * Licensed under the MIT License.
  */
-const path = require('path');
+const pkg = require('../package.json');
 const intercept = require("intercept-stdout");
 const unhook_intercept = intercept(function(txt) {
-    const parentFolder = path.dirname(__dirname).split(path.sep).pop();
-    return `${process.env.VERBOSE === 'verbose' ? `[${parentFolder}] ` : ''}${txt}`;
+    return `${process.env.VERBOSE === 'verbose' ? `[${pkg.name}] ` : ''}${txt}`;
 });
 exports.unhook_intercept = unhook_intercept;
