84188036251386b6edc2dc47cf3da84cb34bf878
Joe <quesjose1@gmail.com>
2018-09-25 16:36:39 -0300

    Automation of nock for mocked tests

--- packages/LUIS/test/luis.cli.test.suite.js
+++ packages/LUIS/test/luis.cli.test.suite.js
@@ -5,29 +5,7 @@ const AppObject = require.resolve('../examples/ApplicationCreateObject');
 const pkg = require('../package.json');
 const nock = require('nock');
 const fs = require('fs-extra');
-// If this is true, then LUIS responses will come from oracle files.
-// If it is false, the LUIS service will be called and if there are changes you will get a new oracle file.
-const mockLuis = true;
-
-function ExpectedPath(file) {
-    return __dirname + "/TestData/" + file;
-}
-
-function GetExpected(oracle, statusCode = 200) {
-    var expected = fs.readJSONSync(oracle);
-    if (mockLuis) {
-        nock('https://westus.api.cognitive.microsoft.com')
-            .post('/luis/api/v2.0/apps/')
-            .reply(statusCode, expected);
-    }
-    return expected;
-}
-
-function TestResponse(file, done) {
-    var expectedPath = ExpectedPath(file);
-    var expected = GetExpected(expectedPath);
-    done(expected);
-}
+const testMockPath = './mockTest';
 
 describe('The LUIS cli tool', () => {
 
@@ -59,13 +37,12 @@ describe('The LUIS cli tool', () => {
             });
 
             it('and all other args', done =>{
-                TestResponse('accessDeniedInvalidSubscription.json', (res)=>{
-                    exec(`node ${luis} --authoringKey dummy-key --region westeurope query --q "message to send" --appId dummy-app`, (error, stdout, stderr) => {
-                        assert.equal(stdout, '');
-                        assert(stderr.includes('401'), stderr);
-                        done();
-                    });
-                }, 401);
+                let mockFile = 'testData.json';
+                exec(`node ${testMockPath} ${mockFile} ${luis} --authoringKey dummy-key --region westeurope query --q "message to send" --appId dummy-app`, (error, stdout, stderr) => {
+                    assert.equal(stdout, '');
+                    assert(stderr.includes('401'));
+                    done();
+                });
             });
         });
         describe('with set command', () => {
@@ -111,13 +88,12 @@ describe('The LUIS cli tool', () => {
             });
 
             it('with ApplicationCreateObject', done =>{
-                TestResponse('accessDeniedInvalidSubscription.json', (res)=>{
-                    exec(`node ${luis} add app --in ${AppObject} --authoringKey dummyKey --region westus`, (error, stdout, stderr) => {
-                        assert.equal(stdout, '');
-                        assert(stderr.includes('Access denied due to invalid subscription key.'));
-                        done();
-                    });
-                }, 401);
+                let mockFile = 'testData.json';
+                exec(`node ${testMockPath} ${mockFile} ${luis} add app --in ${AppObject} --authoringKey dummyKey --region westus`, (error, stdout, stderr) => {
+                    assert.equal(stdout, '');
+                    assert(stderr.includes('Access denied due to invalid subscription key.'));
+                    done();
+                });
             });
         });
     });
