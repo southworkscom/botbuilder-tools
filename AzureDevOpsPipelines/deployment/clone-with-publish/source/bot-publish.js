5f4ed70da4932ec432d4e6dc578d5196f31735d1
VictorGrycuk <victor@grycuk.net>
2019-02-21 15:39:33 -0300

    Add az cli code

---
+++ AzureDevOpsPipelines/deployment/clone-with-publish/source/bot-publish.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("./core");
+class botPublish {
+    constructor() {
+        this.parametersPublish = {
+            'resource-group': 'resource-group',
+            'name': 'name',
+            'proj-name': 'proj-name',
+            'code-dir': 'code-dir',
+            'version': 'version'
+        };
+        this.core = new core_1.core;
+    }
+    botPublish() {
+        const inputs = this.core.GetParameters(this.parametersPublish);
+        this.core.run(`az bot publish ` +
+            `--subscription ${this.core.subscriptionID} ` +
+            `${inputs} ` +
+            `--verbose`);
+    }
+}
+exports.botPublish = botPublish;
---
+++ AzureDevOpsPipelines/deployment/clone-with-publish/source/bot-publish.ts
@@ -0,0 +1,23 @@
+import { core } from "./core";
+
+export class botPublish {
+
+    private parametersPublish = {
+        'resource-group': 'resource-group',
+        'name': 'name',
+        'proj-name': 'proj-name',
+        'code-dir': 'code-dir',
+        'version': 'version'
+    };
+
+    private core: core = new core;
+    
+    public botPublish(): void {
+        const inputs: string = this.core.GetParameters(this.parametersPublish);
+
+        this.core.run(`az bot publish ` +
+            `--subscription ${this.core.subscriptionID} ` +
+            `${inputs} ` +
+            `--verbose`);
+    }
+}
